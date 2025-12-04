5a3afc648b2817246e4e02c340364e251ebcba5e
allison michele ruiz <ruizhernandezallisonmichele@gmail.com>
2025-12-04 11:29:56 -0500

    Agrego LOS NUEVOS CAMBIOS AL GIT

--- SENA_APP/settings.py
+++ SENA_APP/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'bootstrap5',
     'instructores',
     'programas',
-    
+    'cursos',
 ]
 
 MIDDLEWARE = [
---
+++ cursos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Curso, InstructorCurso, AprendizCurso
+
+
+# Register your models here.
+admin.site.register(Curso)
+admin.site.register(InstructorCurso)
+admin.site.register(AprendizCurso)
